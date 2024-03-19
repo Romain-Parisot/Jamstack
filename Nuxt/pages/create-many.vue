4c2bfa5e96be19f5047e21b8cadff824b66e98ec
Rowars2 <romain.parisot@edu.devinci.fr>
2024-03-19 11:58:13 +0100

    chore: add comments about api key

--- Nuxt/pages/create-many.vue
+++ Nuxt/pages/create-many.vue
@@ -6,6 +6,8 @@ export interface Stock {
 }
 
 const MarketFixtures = async () => {
+  // This api key is a free plan, it has a limit of 5 requests per minute, you can use this api but if it reach the limit you can create your own api key for free on https://polygon.io/
+  // The api key should be add in the .env file to secure it, here it is a student project and an free plan api so i put it here to let user test the app without any configuration
   const POLYGON_API_KEY = "B8mglg0lAk13qw7i9gu6n8BEnpb1PHw1";
   const response = await fetch(
     `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2024-03-15?adjusted=true&apiKey=${POLYGON_API_KEY}`
